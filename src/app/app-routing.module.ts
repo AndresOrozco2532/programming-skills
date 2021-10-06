e724a2582ac7619cbb4c88d0fce36bc7f8271603
Diego Andres Orozco Lopez <andresorozco2532@gmail.com>
2021-10-05 19:40:34 -0500

    Chore: add home & auth modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/module/auth.module').then((m) => m.AuthModule),
+  },
+
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/module/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public form: FormGroup;
+  constructor() {}
+
+  ngOnInit(): void {
+    this._setForm();
+  }
+
+  public onSubmit() {}
+
+  private _setForm() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+}
